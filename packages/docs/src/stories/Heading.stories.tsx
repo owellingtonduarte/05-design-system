96165032dbc65dc2627b9792df97f587360463c7
Wellington Duarte <wellx3@live.com>
2023-02-06 22:46:45 -0300

    feat: add core components

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,37 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+
+  args: {
+    children: 'Custom Title',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg', '2xl', '4xl', '5xl', '6xl'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'Heading Text',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão o heading sempre será um `h2`, mas podendo alterar isso com a propriedade `as`',
+      },
+    },
+  },
+}
