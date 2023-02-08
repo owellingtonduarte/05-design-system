bd179e87059e55bdb2dc34a26240f825e6956a72
Wellington Duarte <wellx3@live.com>
2023-02-07 22:34:25 -0300

    ci: Add TurboRepo cache on deploy build

--- packages/docs/src/components/ColorsGrid.tsx
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -1,4 +1,4 @@
-import { colors } from '@ignite-ui/tokens'
+import { colors } from '@dablio-ignite-ui/tokens'
 import { getContrast } from 'polished'
 
 export function ColorsGrid() {
---
+++ packages/ts-config/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @dablio-ignite-ui/ts-config
+
+## 2.0.0
+
+### Major Changes
+
+- Changed the name of project
